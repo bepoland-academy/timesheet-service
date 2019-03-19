a24cc2c024a5f700e3ca3e8369cc3543fd64d637
Wiktor Krzyżanowski <wiktor.krzyanowski1@gmail.com>
2019-03-19 09:35:11 +0100

    Liquibase script created

--- src/main/java/pl/betse/beontime/users/Application.java
+++ src/main/java/pl/betse/beontime/users/Application.java
@@ -2,9 +2,11 @@ package pl.betse.beontime.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@LiquibaseDataSource
 @EnableEurekaClient
 public class Application {
 
